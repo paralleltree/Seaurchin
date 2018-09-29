d877156aeffee029836eedfcd50163617d6fef32
kb10uy <kb10uy@kb10uy.org>
2018-09-29 19:21:30 +0900

    とりあえず作った

---
+++ Cheddeaurchin/SeaurchinComboCounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ched.Components;
+using Ched.Plugins;
+
+namespace Cheddeaurchin
+{
+    [Export(typeof(IScorePlugin))]
+    public class SeaurchinComboCounter : IScorePlugin
+    {
+        public string DisplayName => "コンボ計算 (Seaurchin仕様)";
+
+        public void Run(Score score)
+        {
+            MessageBox.Show("ちんこ");
+        }
+    }
+}
