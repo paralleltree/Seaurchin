8b988f076f89179901ebfce4f75b55a67de0b4a8
Ritsuki Goto <develop@mizucoffee.net>
2018-11-16 21:46:38 +0900

    [add] unzip

--- bootstrap.bat
+++ bootstrap.bat
@@ -3,9 +3,16 @@
 $ANGELSCRIPT_VER = "2.32.0"
 $BOOST_VER = "1.68.0"
 $ZLIB_VER = "1.2.11"
+$LIBPNG_VER = "1.6.34"
+$FMT_VER = "4.1.0"
+$SPDLOG_VER = "0.17.0"
+$GLM_VER = "0.9.9.3"
+$FREETYPE_VER = "2.9.1"
 
 $BOOST_VER_UNDERLINE = $BOOST_VER.Replace(".","_")
 $ZLIB_VER_NUM = $ZLIB_VER.Replace(".","")
+$LIBPNG_VER_NUM = $LIBPNG_VER.Replace(".","")
+$LIBPNG_VER_NUM2 = $LIBPNG_VER_NUM.Substring(0,2)
 
 Write-Host '================================================================================='
 Write-Host ''
@@ -62,6 +69,33 @@ Write-Host ""
 download "https://www.angelcode.com/angelscript/sdk/files/angelscript_$ANGELSCRIPT_VER.zip" "angelscript"
 download "https://dl.bintray.com/boostorg/release/$BOOST_VER/source/boost_$BOOST_VER_UNDERLINE.zip" "boost"
 download "https://zlib.net/zlib$ZLIB_VER_NUM.zip" "zlib"
+download "http://ftp-osl.osuosl.org/pub/libpng/src/libpng$LIBPNG_VER_NUM2/lpng$LIBPNG_VER_NUM.zip" "libpng"
+download "https://github.com/mayah/tinytoml/archive/master.zip" "tinytoml"
+download "https://github.com/fmtlib/fmt/releases/download/$FMT_VER/fmt-$FMT_VER.zip" "fmt"
+download "https://github.com/gabime/spdlog/archive/v$SPDLOG_VER.zip" "spdlog"
+download "https://github.com/g-truc/glm/releases/download/$GLM_VER/glm-$GLM_VER.zip" "glm"
+download "http://dxlib.o.oo7.jp/DxLib/DxLib_VC3_19d.zip" "dxlib"
+download "https://github.com/ubawurinna/freetype-windows-binaries/releases/download/v$FREETYPE_VER/freetype-$FREETYPE_VER.zip" "freetype"
+download "http://us.un4seen.com/files/bass24.zip" "base24"
+download "http://us.un4seen.com/files/z/0/bass_fx24.zip" "base24_fx"
+download "http://us.un4seen.com/files/bassmix24.zip" "base24_mix"
+
+Write-Host "================================================================================="
+Write-Host ""
+Write-Host "* 取得したソースコードを展開します。"
+Write-Host ""
+
+foreach($item in Get-ChildItem "tmp\*.zip"){
+  $name = $item.Name.Split(".")[0]
+  if (!(Test-Path "tmp\$name")) {
+    Write-Host "** $name を展開します。"
+    Write-Host ""
+    Expand-Archive -Path $item -DestinationPath "tmp\$name"
+  } else {
+    Write-Host "** $name は既に展開済なので無視しました。"
+    Write-Host ""
+  }
+}
 
 Write-Host "================================================================================="
 
